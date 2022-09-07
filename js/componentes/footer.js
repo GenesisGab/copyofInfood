a47d3f78841549f1cf834b588da31b8fd9942f34
GenesisGab <78931170+GenesisGab@users.noreply.github.com>
2022-09-07 15:13:07 -0600

    Add files via upload

---
+++ js/componentes/footer.js
@@ -0,0 +1,21 @@
+
+
+
+export function footer (){
+    let root = document.querySelector("#footer");
+  
+   //CREACION DE LOS DIV PARA EL PANEL
+    root.innerHTML = 
+    '<section class="integrantes">'+
+    '<div class="padreIntegrantes">'+
+    '<div></div>'+
+    '<div></div>'+
+    '<div></div>'+
+'</div>'+
+    '</section>'+
+    
+'<section class="infoInfood">'+
+    '<h1>HOLA</h1>'+
+'</section>';
+}
+footer();
